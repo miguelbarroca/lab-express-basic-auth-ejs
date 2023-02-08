4264db155987d32956ea4d04144f765c48bec5ba
Miguel Barroca <miguelbarroca@protonmail.com>
2023-02-08 07:57:17 +0000

    unfinished lab

--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,8 @@
 const { Schema, model } = require("mongoose");
+const validateEmail = function (email) {
+  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  return re.test(email)
+}
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
@@ -6,7 +10,19 @@ const userSchema = new Schema({
     type: String,
     unique: true
   },
-  password: String
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+  }
 });
 
 const User = model("User", userSchema);
